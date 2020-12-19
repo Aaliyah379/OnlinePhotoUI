65343b33375f3fc80eebf21cb6b21dc4db5e7a37
L. M <a.f.girl89@hotmail.com>
2020-12-19 23:03:21 +0200

    Add project files.

---
+++ OnlinePhotoUI/DemoHelper.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Threading;
+
+namespace OnlinePhotoUI
+{
+    internal static class DemoHelper
+    {
+       /// Brief delay to slow browser interaction
+       public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+    }
+}
---
+++ OnlinePhotoUI/OnlineTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace OnlinePhotoUI
+{
+   public class OnlineTest
+    { 
+        [Fact]
+        [Trait ("category","Smoke")]
+        public void LoadingApplicationPage()
+        {
+            using (IWebDriver driver =  new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://passport-photo.online");
+
+                DemoHelper.Pause();
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Фотографии для паспорта и визы онлайн - Passport-Photo.online", pageTitle);
+                Assert.Equal("https://passport-photo.online/ru-ru/blog/", driver.Url);
+            }
+
+        }
+    }
+}
